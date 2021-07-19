49ae1199751b65760d700fbb77785e59ed52daea
Todd <andrewcunningham770@gmail.com>
2021-07-18 22:33:05 -0400

    hey

---
+++ app.js
@@ -0,0 +1,43 @@
+var todd3 = 3;
+var todd7 = 8;
+
+function addNumber(todd3 , todd7){
+  console.log(todd3 + todd7);
+}
+
+
+function subNumber(todd3 , todd7){
+  console.log(todd3 - todd7);
+}
+
+function multiplyNumber(todd3 , todd7){
+  console.log(todd3 * todd7);
+}
+
+
+function divideNumber(todd3 , todd7){
+  console.log(todd3 / todd7);
+}
+
+( addNumber) =>{(todd3 + todd7);}
+
+(subNumber) =>{(todd3 - todd7);}
+
+( multiplyNumber) =>{(todd3 * todd7);}
+
+(divideNumber) =>{(todd3 / todd7);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
